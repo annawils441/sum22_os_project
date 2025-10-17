6deafbeb36df539b2d902b882a2c3169e0f13e0a
annawils441 <ann4.9.4nna@gmail.com>
2025-10-17 10:37:23 +0100

    Version 1 - added a working navbar and styled it.

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+
+# Create Flask application instance
+app = Flask(__name__, template_folder='templates')
+
+# Create Homepage route
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+# Create Login page route
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+# Create Signup page route
+@app.route('/signup')
+def signup_page():
+    return render_template('signup.html')
+
+# Create Rewards page route
+@app.route('/rewards')
+def rewards_page():
+    return render_template('rewards.html')
+
+# Create Resources page route
+@app.route('/resources')
+def resources_page():
+    return render_template('resources.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
